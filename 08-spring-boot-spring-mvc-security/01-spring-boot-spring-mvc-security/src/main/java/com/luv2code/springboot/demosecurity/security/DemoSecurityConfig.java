ba3b56383cdb450e51232dbdba99dd1d89939b9f
beyza <be.bozkurt@outlook.com>
2024-10-16 11:44:16 +0300

    restrict-url-based-on-roles

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -51,6 +51,9 @@ public class DemoSecurityConfig {
                                 .permitAll()
                 )
                 .logout(logout -> logout.permitAll()
+                )
+                .exceptionHandling(configurer ->
+                        configurer.accessDeniedPage("/access-denied")
                 );
 
         return http.build();
