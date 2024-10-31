5d7704278c1770b563423f5662fc1b3ea3201900
beyza <be.bozkurt@outlook.com>
2024-10-31 23:26:54 +0300

    aop

---
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
@@ -0,0 +1,33 @@
+package com.luv2code.aopdemo;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO){
+		return runner -> {
+			demoTheBeforeAdvice(theAccountDAO,theMembershipDAO);
+
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
+		Account a = new Account();
+		theAccountDAO.addAccount(a,true);
+		theAccountDAO.doWork();
+		theMembershipDAO.goToSleep();
+		theMembershipDAO.addMember();
+	}
+
+}
